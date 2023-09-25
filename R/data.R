16f9d7c4edd461f6881240f59e65c9b12989e0ec
jrodu <jordan.rodu@gmail.com>
2023-09-25 14:37:48 -0400

    add running data

--- R/data.R
+++ R/data.R
@@ -47,3 +47,63 @@
 #' }
 #' @source simulated
 "example_data_with_row_col"
+
+#' Simulated data with row and column information
+#'
+#' simulated from a normal distribution-- no signal
+#'
+#' @format ## `running_data`
+#' A data frame with 194314 rows and 48 columns:
+#' \describe{
+#'   \item{run_id}{}
+#'   \item{limb}{}
+#'   \item{timestamp}{}
+#'   \item{step}{}
+#'   \item{stide_pace}{}
+#'   \item{step_rate}{}
+#'   \item{stride_length}{}
+#'   \item{contact_time}{}
+#'   \item{flight_ratio}{}
+#'   \item{power}{}
+#'   \item{shock}{}
+#'   \item{impact_gs}{}
+#'   \item{braking_gs}{}
+#'   \item{footstrike_type}{}
+#'   \item{pronation_excursion_fs_mp}{}
+#'   \item{max_pronation_velocity}{}
+#'   \item{grf_vert_peak}{}
+#'   \item{contact_ratio}{}
+#'   \item{flight_time}{}
+#'   \item{stride_angle}{}
+#'   \item{kleg}{}
+#'   \item{kvert}{}
+#'   \item{grf_vert}{}
+#'   \item{grf_horiz}{}
+#'   \item{swing_force_rate}{}
+#'   \item{total_force_rate}{}
+#'   \item{time_max_fs}{}
+#'   \item{time_fs_mpv}{}
+#'   \item{time_mpv_mp}{}
+#'   \item{time_mp_to}{}
+#'   \item{time_to_min}{}
+#'   \item{time_swing}{}
+#'   \item{medial_lateral_gs}{}
+#'   \item{braking_gs_amp}{}
+#'   \item{impact_gs_amp}{}
+#'   \item{vertical_speed}{}
+#'   \item{elevation_gain}{}
+#'   \item{swing_excursion}{}
+#'   \item{yaw_excursion_fs_mp}{}
+#'   \item{yaw_excursion_mp_to}{}
+#'   \item{yaw_excursion_swing}{}
+#'   \item{stance_velocity_max_fs_mp}{}
+#'   \item{stance_velocity_max_mp_to}{}
+#'   \item{temperature}{}
+#'   \item{step_length}{}
+#'   \item{pronation_excursion_mp_to}{}
+#'   \item{stance_excursion_fs_mp}{}
+#'   \item{stance_excursion_mp_to}{}
+#'   ...
+#' }
+#' @source simulated
+"example_data_with_row_col"
